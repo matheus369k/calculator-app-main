a6b9f416e28afe40dd1b582079b3bf0ca125d24e
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-01-09 18:23:31 -0300

    Bugs...

--- src/server/index.js
+++ src/server/index.js
@@ -36,6 +36,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _a;
+;
 var calcule = {
     calcAll: [],
     caclUmount: [],
@@ -43,9 +44,18 @@ var calcule = {
 };
 var numberCalc = '';
 var barToggleThemer = document.querySelectorAll('.btn');
+function indexOfItems(array, include) {
+    return array.indexOf(include);
+}
+;
+function lastIndexOfItems(array, include) {
+    return array.lastIndexOf(include);
+}
+;
 function includesItems(array, include) {
     return array.includes(include);
 }
+;
 var Numbers = /** @class */ (function () {
     function Numbers(setNumber1, setNumber2) {
         this.number1 = setNumber1;
@@ -90,7 +100,7 @@ var Numbers = /** @class */ (function () {
 // detectando o click...
 barToggleThemer.forEach(function (btn) {
     btn.addEventListener('click', function (e) {
-        // console.log('foreach-btn()')
+        // console.log('foreach-btn()');
         e.stopPropagation();
         if (btn.textContent === null)
             return;
@@ -99,74 +109,95 @@ barToggleThemer.forEach(function (btn) {
 });
 // separando oque faz parte do calculo e oque não....
 function mountCalc(item) {
-    // console.log('mountCalc()')
+    // console.log('mountCalc()');
     if ((item != 'del' &&
         item != 'reset' &&
         ((item != '-' && calcule.caclUmount.length == 0) || (calcule.caclUmount.length != 0))) && (isNaN(parseInt(item)) && (isNaN(Number(calcule.caclUmount[calcule.caclUmount.length - 1]))
         ||
-            calcule.caclUmount.length == 0))) {
+            calcule.caclUmount.length == 0)))
         return;
-    }
     if (includesItems(calcule.caclUmount, 'e') && item != 'reset')
+        return;
+    if (item == '.'
+        && ((calcule.caclUmount.length == 0)
+            ||
+                (lastIndexOfItems(calcule.caclUmount, '.') > lastIndexOfItems(calcule.caclUmount, 'x')
+                    &&
+                        lastIndexOfItems(calcule.caclUmount, '.') > lastIndexOfItems(calcule.caclUmount, '/')
+                    &&
+                        lastIndexOfItems(calcule.caclUmount, '.') > lastIndexOfItems(calcule.caclUmount, '-')
+                    &&
+                        lastIndexOfItems(calcule.caclUmount, '.') > lastIndexOfItems(calcule.caclUmount, '+'))))
         return;
     calcule.caclUmount.push(item);
     AddRemoveScreenElement(calcule.caclUmount, false);
 }
 // fanzendo os calculos por order dos operadores e aparição...
-function Calc() {
-    // console.log('Calc()')
+function OrdenOfTheCalc() {
+    // console.log('Calc()');
     var _this = this;
     calcule.caclUmount = [];
     if (calcule.calcAll.length <= 1) {
         for (var index = 0; index < calcule.calcAll[0].length; index++) {
             calcule.caclUmount.push(calcule.calcAll[0][index]);
         }
+        ;
         AddRemoveScreenElement(calcule.caclUmount, true);
         return;
     }
+    ;
     calcule.calcAll.forEach(function (item, index) { return __awaiter(_this, void 0, void 0, function () {
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
                     if (!(item === 'x')) return [3 /*break*/, 2];
-                    return [4 /*yield*/, compostCalcSide(index)];
+                    return [4 /*yield*/, calcNumbers(index)];
                 case 1:
                     _a.sent();
                     return [3 /*break*/, 4];
                 case 2:
                     if (!(item === '/')) return [3 /*break*/, 4];
-                    return [4 /*yield*/, compostCalcSide(index)];
+                    return [4 /*yield*/, calcNumbers(index)];
                 case 3:
                     _a.sent();
                     _a.label = 4;
                 case 4:
-                    if (!(!includesItems(calcule.calcAll, 'x') && !includesItems(calcule.calcAll, '/'))) return [3 /*break*/, 8];
+                    if (!(!includesItems(calcule.calcAll, 'x') && !includesItems(calcule.calcAll, '/'))) return [3 /*break*/, 9];
                     if (!(item === '+')) return [3 /*break*/, 6];
-                    return [4 /*yield*/, compostCalcSide(index)];
+                    return [4 /*yield*/, calcNumbers(index)];
                 case 5:
                     _a.sent();
                     return [3 /*break*/, 8];
                 case 6:
                     if (!(item === '-')) return [3 /*break*/, 8];
-                    return [4 /*yield*/, compostCalcSide(index)];
+                    return [4 /*yield*/, calcNumbers(index)];
                 case 7:
                     _a.sent();
                     _a.label = 8;
-                case 8: return [2 /*return*/];
+                case 8:
+                    ;
+                    _a.label = 9;
+                case 9:
+                    ;
+                    return [2 /*return*/];
             }
         });
     }); });
 }
+;
 // Organizando os dados obtidos em uma nova Array para calcular o valor...
 (_a = document.querySelector('#result')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function (e) {
-    // console.log('result-addeventlistener()')
+    // console.log('result-addeventlistener()');
     e.stopPropagation();
     calcule.caclUmount.push('=');
     for (var index = 0; index < calcule.caclUmount.length; index++) {
-        if (parseInt(calcule.caclUmount[index]) ||
-            parseInt(calcule.caclUmount[index]) == 0 ||
-            calcule.caclUmount[index] == '.' ||
-            (calcule.caclUmount[index] == '-' && index == 0)) {
+        if (parseInt(calcule.caclUmount[index])
+            ||
+                parseInt(calcule.caclUmount[index]) == 0
+            ||
+                calcule.caclUmount[index] == '.'
+            ||
+                (calcule.caclUmount[index] == '-' && index == 0)) {
             numberCalc = "".concat(numberCalc).concat(calcule.caclUmount[index]);
         }
         else if (!Number(calcule.caclUmount[index])) {
@@ -175,25 +206,27 @@ function Calc() {
             calcule.calcAll.push(calcule.caclUmount[index]);
             numberCalc = '';
         }
+        ;
     }
+    ;
     if (includesItems(calcule.caclUmount, '='))
         calcule.caclUmount.pop();
     if (includesItems(calcule.calcAll, '='))
         calcule.calcAll.pop();
     if (Number(calcule.caclUmount[calcule.caclUmount.length - 1]))
-        Calc();
+        OrdenOfTheCalc();
 });
 // calculando
-function compostCalcSide(index) {
+function calcNumbers(index) {
     return __awaiter(this, void 0, void 0, function () {
         var number1, number2, numbers, numberremove;
         return __generator(this, function (_a) {
-            // console.log('compostCalcSide()')
+            // console.log('compostCalcSide()');
             calcule.result = 0;
             number1 = Number(calcule.calcAll[index - 1]);
             number2 = Number(calcule.calcAll[index + 1]);
             numbers = new Numbers(number1, number2);
-            // console.log(calcule.calcAll)
+            //console.log(calcule.calcAll);
             switch (calcule.calcAll[index]) {
                 case '/':
                     calcule.result = numbers.divi;
@@ -211,14 +244,15 @@ function compostCalcSide(index) {
                     break;
             }
             numberremove = calcule.calcAll.splice(index - 1, 3, "".concat(calcule.result));
-            Calc();
+            // console.log(numberremove)
+            OrdenOfTheCalc();
             return [2 /*return*/];
         });
     });
 }
 // Adicionar a tela os valores digitados e resultados
 function AddRemoveScreenElement(textElement, confimation) {
-    // console.log('AddRemoveScreenElement()')
+    // console.log('AddRemoveScreenElement()');
     var screenContainer = document.getElementById('screen');
     if (screenContainer === null)
         return;
@@ -248,23 +282,28 @@ function AddRemoveScreenElement(textElement, confimation) {
                 breakLineMet = '\n';
                 breakLineCont += 19;
             }
+            ;
             screenContainer.innerText += "".concat(textElement[index]).concat(breakLineMet);
         }
         else {
             screenContainer.innerText += "".concat(textElement[index]);
         }
+        ;
     }
+    ;
     if (textElement.length < 15) {
         screenContainer.style.fontSize = '1.6em';
         breakLineCont = 14;
     }
+    ;
     if (calcule.caclUmount.length == 0)
         screenContainer.innerText = '';
 }
+;
 // switch themer
 document.querySelectorAll('.switchThemerbtn').forEach(function (btn, index) {
     btn.addEventListener('click', function () {
-        // console.log(document.body.className)
+        // console.log(document.body.className);
         var _a, _b;
         document.body.classList.replace("".concat(document.body.className), "themer-".concat(index + 1, "-active"));
         (_a = document.querySelector('.active')) === null || _a === void 0 ? void 0 : _a.classList.remove('active');
--- src/server/index.ts
+++ src/server/index.ts
@@ -5,7 +5,7 @@ interface Calcule {
     calcAll: string[];
     caclUmount: string[];
     result: number;
-}
+};
 
 let calcule: Calcule = {
     calcAll: [],
@@ -20,10 +20,17 @@ let numberCalc: string = '';
 const barToggleThemer = document.querySelectorAll('.btn');
 
 
+function indexOfItems(array: string[], include: string): number {
+    return array.indexOf(include);
+};
 
-function includesItems(array: string[] ,include: string): boolean {
+function lastIndexOfItems(array: string[], include: string): number {
+    return array.lastIndexOf(include);
+};
+
+function includesItems(array: string[], include: string): boolean {
     return array.includes(include);
-}
+};
 
 
 
@@ -58,19 +65,20 @@ class Numbers {
 
 barToggleThemer.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        // console.log('foreach-btn()')
+        // console.log('foreach-btn()');
 
         e.stopPropagation();
 
-        if (btn.textContent === null) return
+        if (btn.textContent === null) return;
 
-        mountCalc(btn.textContent)
+        mountCalc(btn.textContent);
     });
 });
 
 // separando oque faz parte do calculo e oque não....
+
 function mountCalc(item: string) {
-    // console.log('mountCalc()')
+    // console.log('mountCalc()');
 
     if (
         (
@@ -93,49 +101,67 @@ function mountCalc(item: string) {
                 calcule.caclUmount.length == 0
             )
         )
-    ) {
+    ) return;
 
-        return
+    if (includesItems(calcule.caclUmount, 'e') && item != 'reset') return;
 
-    }
+    if (
+        item == '.'
+        && (
+            (
+                calcule.caclUmount.length == 0
+            )
+            ||
+            (
+                lastIndexOfItems(calcule.caclUmount, '.') > lastIndexOfItems(calcule.caclUmount, 'x')
+                &&
+                lastIndexOfItems(calcule.caclUmount, '.') > lastIndexOfItems(calcule.caclUmount, '/')
+                &&
+                lastIndexOfItems(calcule.caclUmount, '.') > lastIndexOfItems(calcule.caclUmount, '-')
+                &&
+                lastIndexOfItems(calcule.caclUmount, '.') > lastIndexOfItems(calcule.caclUmount, '+')
+            )
+        )
 
-    if (includesItems(calcule.caclUmount, 'e') && item != 'reset') return
+    ) return;
 
-    calcule.caclUmount.push(item)
+    calcule.caclUmount.push(item);
 
-    AddRemoveScreenElement(calcule.caclUmount, false)
+    AddRemoveScreenElement(calcule.caclUmount, false);
 
 }
 
 // fanzendo os calculos por order dos operadores e aparição...
-function Calc() {
-    // console.log('Calc()')
 
-    calcule.caclUmount = []
+function OrdenOfTheCalc() {
+    // console.log('Calc()');
+
+    calcule.caclUmount = [];
 
 
     if (calcule.calcAll.length <= 1) {
 
         for (let index = 0; index < calcule.calcAll[0].length; index++) {
 
-            calcule.caclUmount.push(calcule.calcAll[0][index])
+            calcule.caclUmount.push(calcule.calcAll[0][index]);
 
-        }
+        };
 
-        AddRemoveScreenElement(calcule.caclUmount, true)
+        AddRemoveScreenElement(calcule.caclUmount, true);
 
-        return
+        return;
 
-    }
+    };
+
     calcule.calcAll.forEach(async (item, index) => {
 
         if (item === 'x') {
 
-            await compostCalcSide(index)
+            await calcNumbers(index);
 
         } else if (item === '/') {
 
-            await compostCalcSide(index)
+            await calcNumbers(index);
 
         }
 
@@ -143,131 +169,134 @@ function Calc() {
 
             if (item === '+') {
 
-                await compostCalcSide(index)
+                await calcNumbers(index);
 
             } else if (item === '-') {
 
-                await compostCalcSide(index)
+                await calcNumbers(index);
 
-            }
-        }
+            };
+        };
 
-    })
-}
+    });
+};
 
 
 // Organizando os dados obtidos em uma nova Array para calcular o valor...
 document.querySelector('#result')?.addEventListener('click', (e) => {
-    // console.log('result-addeventlistener()')
+    // console.log('result-addeventlistener()');
 
     e.stopPropagation();
 
-    calcule.caclUmount.push('=')
+    calcule.caclUmount.push('=');
 
     for (let index = 0; index < calcule.caclUmount.length; index++) {
 
         if (
-            parseInt(calcule.caclUmount[index]) ||
-            parseInt(calcule.caclUmount[index]) == 0 ||
-            (calcule.caclUmount[index] == '.' && 
-            calcule.caclUmount[index-1] != '.' &&
-            calcule.caclUmount[index-2] != '.' &&
-            calcule.caclUmount[index-3] != '.'
-            ) ||
+            parseInt(calcule.caclUmount[index])
+            ||
+            parseInt(calcule.caclUmount[index]) == 0
+            ||
+            calcule.caclUmount[index] == '.'
+            ||
             (
                 calcule.caclUmount[index] == '-' && index == 0
             )
         ) {
 
-            numberCalc = `${numberCalc}${calcule.caclUmount[index]}`
+            numberCalc = `${numberCalc}${calcule.caclUmount[index]}`;
 
         } else if (!Number(calcule.caclUmount[index])) {
 
-            if (numberCalc) calcule.calcAll.push(numberCalc)
+            if (numberCalc) calcule.calcAll.push(numberCalc);
 
-            calcule.calcAll.push(calcule.caclUmount[index])
+            calcule.calcAll.push(calcule.caclUmount[index]);
 
             numberCalc = '';
-        }
+        };
 
-    }
+    };
 
-    if (includesItems(calcule.caclUmount, '=')) calcule.caclUmount.pop()
-    if (includesItems(calcule.calcAll, '=')) calcule.calcAll.pop()
+    if (includesItems(calcule.caclUmount, '=')) calcule.caclUmount.pop();
+    if (includesItems(calcule.calcAll, '=')) calcule.calcAll.pop();
 
-    if (Number(calcule.caclUmount[calcule.caclUmount.length - 1])) Calc()
+    if (Number(calcule.caclUmount[calcule.caclUmount.length - 1])) OrdenOfTheCalc();
 
 });
 
+
 // calculando
-async function compostCalcSide(index: number) {
-    // console.log('compostCalcSide()')
+
+async function calcNumbers(index: number) {
+    // console.log('compostCalcSide()');
 
     calcule.result = 0;
 
     const number1 = Number(calcule.calcAll[index - 1]);
     const number2 = Number(calcule.calcAll[index + 1]);
 
-    const numbers = new Numbers(number1, number2)
+    const numbers = new Numbers(number1, number2);
 
 
-    // console.log(calcule.calcAll)
+    //console.log(calcule.calcAll);
 
     switch (calcule.calcAll[index]) {
 
         case '/':
 
-            calcule.result = numbers.divi
-            break
+            calcule.result = numbers.divi;
+            break;
 
         case 'x':
 
-            calcule.result = numbers.mult
-            break
+            calcule.result = numbers.mult;
+            break;
 
         case '+':
 
-            calcule.result = numbers.sum
-            break
+            calcule.result = numbers.sum;
+            break;
 
         case '-':
 
-            calcule.result = numbers.subtr
-            break
+            calcule.result = numbers.subtr;
+            break;
 
         default:
-            break
+            break;
     }
 
-    const numberremove = calcule.calcAll.splice(index - 1, 3, `${calcule.result}`)
+    const numberremove = calcule.calcAll.splice(index - 1, 3, `${calcule.result}`);
 
-    Calc()
+    // console.log(numberremove)
+
+    OrdenOfTheCalc();
 }
 
 // Adicionar a tela os valores digitados e resultados
 function AddRemoveScreenElement(textElement: string[], confimation: boolean) {
-    // console.log('AddRemoveScreenElement()')
+    // console.log('AddRemoveScreenElement()');
 
     const screenContainer: HTMLElement | null = document.getElementById('screen');
 
-    if (screenContainer === null) return
+    if (screenContainer === null) return;
 
-    if (confimation) calcule.calcAll = []
+    if (confimation) calcule.calcAll = [];
 
     let breakLineCont: number = 14;
     let breakLineMet: string = '';
 
     for (const index in textElement) {
 
-        if (textElement[index] == '=') continue
+        if (textElement[index] == '=') continue;
 
         if (includesItems(textElement, 'del')) calcule.caclUmount.splice(textElement.length - 2, textElement.length);
 
         if (includesItems(textElement, 'reset')) {
 
-            calcule.caclUmount = []
+            calcule.caclUmount = [];
 
-            break
+            break;
 
         } else if (parseInt(index) === 0) {
 
@@ -275,19 +304,19 @@ function AddRemoveScreenElement(textElement: string[], confimation: boolean) {
 
         } else if (parseInt(index) === breakLineCont) {
 
-            breakLineMet = ''
-            
-            if (parseInt(index) == 14){
+            breakLineMet = '';
+
+            if (parseInt(index) == 14) {
 
                 screenContainer.style.fontSize = '1.2em';
-                breakLineCont = 18
+                breakLineCont = 18;
 
             } else {
 
-                breakLineMet = '\n'
-                breakLineCont += 19
+                breakLineMet = '\n';
+                breakLineCont += 19;
 
-            }
+            };
 
             screenContainer.innerText += `${textElement[index]}${breakLineMet}`;
 
@@ -295,32 +324,33 @@ function AddRemoveScreenElement(textElement: string[], confimation: boolean) {
 
             screenContainer.innerText += `${textElement[index]}`;
 
-        }
-    }
+        };
+    };
 
     if (textElement.length < 15) {
 
         screenContainer.style.fontSize = '1.6em';
-        breakLineCont = 14
+        breakLineCont = 14;
 
-    }
+    };
 
     if (calcule.caclUmount.length == 0) screenContainer.innerText = '';
 
-}
+};
 
 
 
 // switch themer
-document.querySelectorAll('.switchThemerbtn').forEach((btn,index) =>{
-    btn.addEventListener('click', () =>{
-        // console.log(document.body.className)
+
+document.querySelectorAll('.switchThemerbtn').forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        // console.log(document.body.className);
 
         document.body.classList.replace(`${document.body.className}`, `themer-${index + 1}-active`);
 
         document.querySelector('.active')?.classList.remove('active');
 
-        document.querySelector(`.themer-${index+1}`)?.classList.add('active')
+        document.querySelector(`.themer-${index + 1}`)?.classList.add('active');
 
-    })
-})
+    });
+});
